03640d3c264a8d7dfee267e6581ab9919d3d39b5
Ibrahima sory TRAORE <ibrahima982011@hotmail.fr>
2019-04-20 18:11:01 +0000

    Création du Microservice-expedition

---
+++ microservice-expedition/src/main/java/com/expedition/MicroserviceExpeditionApplication.java
@@ -0,0 +1,13 @@
+package com.expedition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceExpeditionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceExpeditionApplication.class, args);
+	}
+
+}
